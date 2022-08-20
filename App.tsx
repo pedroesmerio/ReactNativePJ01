51aa067ceda777cae69544b52b86c1fea13c6364
Pedro Esmerio <p.h.esmerio@gmail.com>
2022-08-20 00:15:52 -0300

    Feat: Creating/Styling Statusbar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
+import { StatusBar } from "expo-status-bar"
+
 import { Home } from "./src/screens/Home";
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <StatusBar
+        style="inverted"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
   )
 }
